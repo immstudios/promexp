b5be655a118ebaf9feb71ce24724c975be7370ad
Martin Wacker <martas@imm.cz>
2021-02-04 05:52:11 -0800

    Added storagespaces health provider

--- promexp/providers/__init__.py
+++ promexp/providers/__init__.py
@@ -2,10 +2,12 @@ from .psutil import PSUtilProvider
 from .nvidia import NVIDIAProvider
 from .storage import StorageProvider
 from .network import NetworkProvider
+from .storagespaces import StorageSpacesProvider
 
 registry = [
     PSUtilProvider,
     NVIDIAProvider,
     StorageProvider,
-    NetworkProvider
+    NetworkProvider,
+    StorageSpacesProvider,
 ]
---
+++ promexp/providers/storagespaces.py
@@ -0,0 +1,83 @@
+__all__ = ["StorageSpacesProvider"]
+
+import os
+import subprocess
+
+from ..provider import BaseProvider
+
+
+def get_ss_status():
+    c = subprocess.Popen([
+        "powershell.exe", 
+        "Get-VirtualDisk | Format-Table FriendlyName,ResiliencySettingName,OperationalStatus, HealthStatus"
+        ], 
+        stderr=subprocess.PIPE, 
+        stdout=subprocess.PIPE
+    )
+    stdout, stderr = c.communicate()
+
+    bounds = False
+    result = []
+    for line in stdout.decode("utf-8").split("\n"):
+        if line.startswith("---"):
+            bounds = [0]
+            while True:
+                pos = line.find(" -", bounds[-1])
+                if pos == -1:
+                    break
+                bounds.append(pos+1)
+            continue
+
+        if not bounds:
+            continue
+
+        line = line.strip()
+        if not line:
+            continue
+
+        title = line[bounds[0]:bounds[1]].strip()
+        mode = line[bounds[1]:bounds[2]].strip()
+        status = line[bounds[2]:bounds[3]].strip()
+        health = line[bounds[3]:].strip()
+
+        result.append({
+            "title" : title,
+            "mode" : mode,
+            "status" : status,
+            "health" : health
+        })
+        return result
+
+
+class StorageSpacesProvider(BaseProvider):
+    name = "storagespaces"
+
+    def __init__(self, parent, settings):
+        super(StorageSpacesProvider, self).__init__(parent, settings)
+
+        if os.name != "nt":        
+            self.disable()
+            return
+
+        try:
+            status = get_ss_status()
+        except Exception:
+            status = []
+
+        if not status:
+            self.disable()
+
+
+    def collect(self):
+        try:
+            status = get_ss_status()
+        except Exception:
+            status = []
+
+
+        for sspace in status:
+            tags = {
+                "title" : sspace["title"],
+                "mode" : sspace["mode"]
+            }
+            self.add("storage_space_healthy", int(sspace["health"] == "Healthy"), **tags)
