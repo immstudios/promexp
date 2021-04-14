7c3c5798cb94a3f311669f027f73db828d69d8ce
Martastain <martas@imm.cz>
2021-04-14 17:12:56 +0200

    Added setup, version bump to 1.0.0 (as uploaded to PyPi)

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "promexp",
+    version = "1.0.0",
+    author = "Martin Wacker",
+    author_email = "martas@imm.cz",
+    description = "A simple system metrics exporter for Prometheus",
+    license = "MIT",
+    keywords = "metrics devops prometheus",
+    url = "https://github.com/immstudios/promexp",
+    packages=['promexp', 'promexp.providers', 'promexp.providers.casparcg'],
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ],
+)
